a58bd3921ffcf9a49f9646e85e9257af5ff89ea4
Larissa Marns <lari.marins9803@gmail.com>
2023-07-14 11:53:36 -0300

    seçao hero, componete titulo e quadro

--- assets/components/titulo.js
+++ assets/components/titulo.js
@@ -4,10 +4,8 @@ export class Titulo extends LitElement {
   static styles = [
     css`
       :host {
-          display: block;
-       
-          color: var(--cor-secundaria, #665e57);
-       
+        display: block;      
+        color: var(--cor-secundaria, #665e57);
         text-shadow: 2px 2px  0px rgba(0, 0, 0, 0.25);
         font-family: var(--fonte-titulo);
         font-size: 3rem;
@@ -15,11 +13,17 @@ export class Titulo extends LitElement {
         font-weight: 700;
         line-height: 100%; /* 48px */
       }
+
+
+      ::slotted(*){
+        font-size: inherit;
+        margin:0;
+      }
     `,
   ];
 
   render() {
-    return html`Título`;
+    return html`<slot>Título</slot>`;
   }
 }
 customElements.define("app-titulo", Titulo);
