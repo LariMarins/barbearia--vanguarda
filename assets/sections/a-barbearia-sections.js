396b3e4280e1ff39db5fabd9bf232eb82ed70e2e
LariMarins <lari.marins9803@gmail.com>
2023-07-18 08:26:10 -0300

    seção a barbearia

--- assets/sections/a-barbearia-sections.js
+++ assets/sections/a-barbearia-sections.js
@@ -18,19 +18,55 @@ export class ABarbeariaSections extends LitElement {
         gap: 10px;
 
         background: var(--tom1, #ead8c7);
+
+        border: 3px solid;
+        border-image-source: linear-gradient(
+          90deg,
+          rgba(102, 78, 56, 0) 50%,
+          rgba(102, 78, 56, 1) 100%
+        );
+        border-image-slice: 1;
+
+        border-left: 0;
+        border-bottom: 0;
       }
 
       app-paragrafo {
-        width:270px;
-       
-        color: var(--cor-secundaria, #665e57);
+        width: 270px;
+
+        
         text-align: justify;
-        font-family: var(--fonte-corpo);
-        font-size: 1rem;
-        font-style: normal;
-        font-weight: 400;
         line-height: 140%; /* 22.4px */
-        letter-spacing: -1.28px;
+        letter-spacing: -1.424px;
+      }
+
+      app-quadro {
+        width: 335px;
+        height: 335px;
+        border-radius: 100%;
+
+        position: absolute;
+        right: -82px;
+        top: 106px;
+        z-index: -10;
+      }
+      app-bolinhas {
+        display: flex;
+        width: 300px;
+        height: 300px;
+        align-items: flex-start;
+        gap: 10px;
+       
+        position: absolute;
+        left: -150px;
+        top: 72px;
+        z-index:-20;
+
+        background-color: var(--tom3);
+      }
+
+      img{
+        width:100%;
       }
     `,
   ];
@@ -38,6 +74,10 @@ export class ABarbeariaSections extends LitElement {
   render() {
     return html`
       <app-titulo> A Barbearia</app-titulo>
+      <app-bolinhas></app-bolinhas>
+      <app-quadro>
+        <img src="../../public/a-barbearia.jpg" alt= " a barbearia">
+      </app-quadro>
       <article>
         <app-paragrafo>
           Bem-vindo à Barbearia Vanguarda, onde tradição e inovação se encontram
--- assets/styles/sectioon-style.js
+++ assets/styles/sectioon-style.js
@@ -14,5 +14,6 @@ export const section = css`
 
     padding: 2rem;
     overflow:hidden;
+    position:relative;
   }
 `;
